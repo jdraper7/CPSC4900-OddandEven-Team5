943f495f371fc517af7cf1357b972f0a11daec0f
jdraper7 <jdrapergreen@gmail.com>
2021-03-04 18:25:58 -0500

    Class file for main menu

--- GamePlatform.java
+++ GamePlatform.java
@@ -1,5 +1,7 @@
 public class GamePlatform 
 {
     // This will be the main menu of the entire game library.
+    
     // It likely possible to create this using the same logic that generates the GUI in the two solitaire.java games and the logic that creates the buttoms at the 
+    // bottom of each game session.
 }
