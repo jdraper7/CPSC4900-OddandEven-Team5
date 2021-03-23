49041aa104e1f379d470262f79ccf51571ce1480
JesusP <ckf782@mocs.utc.edu>
2021-03-23 18:04:16 -0400

    test commit

--- EOCardTest.java
+++ EOCardTest.java
@@ -56,4 +56,56 @@ class EOCardTest {
     @Test
     void paintComponent() {
     }
+
+    @org.junit.jupiter.api.Test
+    void testGetSuit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetWhereAmI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetWhereAmI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetXY() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetFaceStatus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetXY() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetSuit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetFaceup() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetFacedown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testContains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testPaintComponent() {
+    }
 }
