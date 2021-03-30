81ac952d8b811cd3a634c2acce8f0fd65f59ac92
jesus patino <eljesus788@gmailcom>
2021-03-30 11:20:29 -0400

    TEST(EOSolitaire): Increase test coverage of EOSolitaire

--- EOSolitaireTest.java
+++ EOSolitaireTest.java
@@ -1,26 +1,57 @@
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EOSolitaireTest {
-
     @Test
     void moveCard() {
+        int x = 0;
+        int y = 1;
+        EOCard c = new EOCard();
+        c.setBounds(new Rectangle(new Point(x, y), new Dimension(EOCard.CARD_WIDTH + 10, EOCard.CARD_HEIGHT + 10)));
+        c.setXY(new Point(x, y));
+        assertEquals(x, c.getX());
+        assertEquals(y, c.getY());
     }
 
     @Test
     void setScore() {
+        JFrame gpFrame = new JFrame("Even and Odd Solitaire");;
+        EOSolitaire test = new EOSolitaire(0, 0 , gpFrame);
+        test.setScore(10);
+        assertEquals(10, test.score);
     }
 
     @Test
     void updateTimer() {
+        // initialize
+        JFrame gpFrame = new JFrame("Even and Odd Solitaire");;
+        EOSolitaire test = new EOSolitaire(0, 0 , gpFrame);
+        test.updateTimer();
+        assertEquals(1, test.time);
     }
+
 
     @Test
     void startTimer() {
+        JFrame gpFrame = new JFrame("Even and Odd Solitaire");;
+        EOSolitaire test = new EOSolitaire(0, 0 , gpFrame);
+        test.startTimer();
+        assertSame(true, test.timeRunning);
+
     }
 
     @Test
     void toggleTimer() {
+        JFrame gpFrame = new JFrame("Even and Odd Solitaire");;
+        EOSolitaire test = new EOSolitaire(0, 0 , gpFrame);
+        assertSame(true, test.timeRunning);
+        test.toggleTimer();
+        assertSame(false, test.timeRunning);
+        test.toggleTimer();
+        assertSame(true, test.timeRunning);
     }
 }
