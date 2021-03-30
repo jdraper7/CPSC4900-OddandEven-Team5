6e4c8a50bcc578cf69d97214d0b40aa23036965e
Ariana <arianayerg@gmail.com>
2021-03-30 11:20:13 -0400

    TEST(EOCardTest): Increase test coverage of EOCardTest

--- EOCardTest.java
+++ EOCardTest.java
@@ -1,111 +1,143 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EOCardTest {
+    @BeforeAll
+    static void setUp(){
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+    }
 
-    @Test
+
+
+    @org.junit.jupiter.api.Test
+
+
+    /*
+     * States in Main EO Card class that method is never used
+     */
     void getSuit() {
-
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        assertEquals(card.getSuit(), EOCard.Suit.HEARTS);
     }
 
     @Test
+    /*
+     *
+     */
     void getValue() {
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        assertEquals(card.getValue(), EOCard.Value.ACE );
     }
 
     @Test
+    /*
+     *
+     */
     void setWhereAmI() {
+        Point p = new Point(10, 15);
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        card.setWhereAmI(p);
+        assertEquals(card.getWhereAmI(), p);
     }
 
     @Test
+    /*
+     *
+     */
     void getWhereAmI() {
+        Point p = new Point(10, 15);
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        card.setWhereAmI(p);
+        assertEquals(card.getWhereAmI(), p);
     }
 
     @Test
+    /*
+     *
+     */
     void getXY() {
+//        Point m = new Point(10, 15);
+//        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+//        card.setWhereAmI(m);
+//        assertEquals(m.get, m);
     }
 
     @Test
+    /*
+     *
+     */
     void getFaceStatus() {
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        assertFalse(card.getFaceStatus());
     }
 
     @Test
+    /*
+     *
+     */
     void setXY() {
+//        Point m = new Point(10,15);
+//        Point P = new Point(20, 25);
+//        setXY(m)
     }
 
     @Test
+    /*
+     *
+     */
     void setSuit() {
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        card.setSuit(EOCard.Suit.CLUBS);
+        assertSame(card.getSuit(), EOCard.Suit.CLUBS);
     }
 
     @Test
+    /*
+     *
+     */
     void setValue() {
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        card.setValue(EOCard.Value.TWO);
+        assertEquals(card.getValue(), EOCard.Value.TWO);
     }
 
     @Test
+    /*
+     *
+     */
     void setFaceup() {
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        card.setFacedown();
+        card.setFaceup();
+        assertTrue(card.getFaceStatus());
     }
 
     @Test
+    /*
+     *
+     */
     void setFacedown() {
+        EOCard card = new EOCard(EOCard.Suit.HEARTS, EOCard.Value.ACE);
+        card.setFacedown();
+        assertFalse(card.getFaceStatus());
     }
 
     @Test
+    /*
+     * Unable to complete testing, will test via practical method
+     */
     void contains() {
+
+        
     }
 
     @Test
+    /*
+     * Unable to complete testing, will test via practical method
+     */
     void paintComponent() {
     }
 
-    @org.junit.jupiter.api.Test
-    void testGetSuit() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testGetValue() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testSetWhereAmI() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testGetWhereAmI() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testGetXY() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testGetFaceStatus() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testSetXY() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testSetSuit() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testSetValue() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testSetFaceup() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testSetFacedown() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testContains() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testPaintComponent() {
-    }
 }
