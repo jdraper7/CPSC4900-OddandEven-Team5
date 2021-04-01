8f187c8fde9a72c98e16a423d480021131ccc56e
jdraper7 <48225753+jdraper7@users.noreply.github.com>
2021-04-01 18:28:10 -0400

    Update GamePlatformTest.java

--- GamePlatformTest.java
+++ GamePlatformTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GamePlatformTest
@@ -14,36 +15,48 @@ class GamePlatformTest
     @Test
     void saveEOScore()
     {
-
+        ArrayList<String[]> runningEOStats = new ArrayList<>();
+        runningEOStats.add(new String[]{});
+        assertEquals(1,1);
     }
 
     @Test
     void saveKScore()
     {
-
+        ArrayList<String[]> runningKStats = new ArrayList<>();
+        runningKStats.add(new String[]{});
+        assertEquals(1,1);
     }
 
     @Test
     void saveEWScore()
     {
-
+        ArrayList<String[]> runningEWStats = new ArrayList<>();
+        runningEWStats.add(new String[]{});
+        assertEquals(1,1);
     }
 
     @Test
     void saveEScore()
     {
-
+        ArrayList<String[]> runningEStats = new ArrayList<>();
+        runningEStats.add(new String[]{});
+        assertEquals(1,1);
     }
 
     @Test
     void saveEOffScore()
     {
-
+        ArrayList<String[]> runningEOffStats = new ArrayList<>();
+        runningEOffStats.add(new String[]{});
+        assertEquals(1,1);
     }
 
     @Test
     void saveExScore()
     {
-
+        ArrayList<String[]> runningExStats = new ArrayList<>();
+        runningExStats.add(new String[]{});
+        assertEquals(1,1);
     }
 }
